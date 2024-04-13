caf13e53794b9d0e9cb6993d23b3828f50ae9ee0
ahnaf daiyan <bartamitdaiyan@gmail.com>
2024-04-13 19:31:21 +0600

    auth redirect and env variables

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,17 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
+console.log('Inside firebase cofig file:  ',import.meta.env.VITE_PASS);
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBlAzcKFa5sqviz7kxtrIrCe3qtnbtnPWE",
-  authDomain: "m9-m52-dragonnews-auth.firebaseapp.com",
-  projectId: "m9-m52-dragonnews-auth",
-  storageBucket: "m9-m52-dragonnews-auth.appspot.com",
-  messagingSenderId: "165166535997",
-  appId: "1:165166535997:web:d5f3f049af06d1cb5e2154"
+  apiKey: import.meta.env.VITE_apikey,
+  authDomain:import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket:import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId:import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
